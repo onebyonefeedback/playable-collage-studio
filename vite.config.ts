a1660697a6f949a8ad11cb3b36f5c4082337df80
GrUnD <teneschevgrund@gmail.com>
2025-11-21 23:46:36 +0700

    [DEV] Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-
-  
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: './',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
